795e75b3883c19efdcc4cb4e757439187d24872c
anirudhkaluri <anirudhkaluri@Anirudh-Kaluris-Mac.local>
2023-03-12 17:31:38 +0530

    spring web, rest repo, data jpa, h2 added as dependencies

---
+++ src/main/java/com/microservices/TourWebsite/TourWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.TourWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourWebsiteApplication.class, args);
+	}
+
+}
